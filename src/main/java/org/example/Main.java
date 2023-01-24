5a975783ca02544e936a05e2a3f647cfff9b6440
Piotr Petryka <ppwxp19@gmail.com>
2023-01-24 14:59:14 +0100

    Simulation is with bug, but made. I ve got some problems with inicial logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,23 @@
 package org.example;
 
-import org.example.powerplants.CoalPlant;
+import org.example.consumers.EnergyConsumers;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Simulation simulation = new Simulation();
-        simulation.addCoalMines(10);
-        System.out.println(simulation.getFuelUsedPerMonth());
+        ResourceStorage resourceStorage = new ResourceStorage();
+        resourceStorage.addCoalMines(100);
+        resourceStorage.addCoalPlants(10);
+        resourceStorage.addUraniumMine(1);
+        resourceStorage.addSolarPlants(10);
+        int energyProduced = resourceStorage.getEnergyProduced() - resourceStorage.getFuelUsedPerMonth();
+        if (EnergyConsumers.WORLD.getConsumedUnitsPerDay() * 30 <= energyProduced){
+            System.out.println("Dont enough energy produced");
+        }else{
+            System.out.println("Energy Consumed in WORLD:" + EnergyConsumers.WORLD.getConsumedUnitsPerDay());
+            System.out.println("Energy Produced: " + energyProduced);
+            System.out.println("Units consumed: " + resourceStorage.getConsumedFuel());
+
+        }
+        System.out.println(energyProduced + " " + EnergyConsumers.WORLD.getConsumedUnitsPerDay() *30);
     }
 }
--- src/main/java/org/example/ResourceStorage.java
+++ src/main/java/org/example/ResourceStorage.java
@@ -1,9 +1,70 @@
 package org.example;
 
+import org.example.mines.CoalMine;
+import org.example.mines.Mine;
+import org.example.mines.UraniumMine;
+import org.example.powerplants.CoalPlant;
+import org.example.powerplants.PowerPlant;
+import org.example.powerplants.SolarPlant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResourceStorage {
-    private int fuelGot;
-    private int fuelConsumed;
-    private int energyProduced;
+//    private int fuelGot = getConsumedFuel();
+//    private int fuelConsumed = getFuelUsedPerMonth();
+//    private int energyProduced = getEnergyProduced();
+    private List<Mine> minesList = new ArrayList<>();
+    private List<PowerPlant> powerPlants = new ArrayList<>();
+
+    public void addCoalMines(int numberOfMines){
+        for (int i=0; i <= numberOfMines; i++){
+            CoalMine coalMine = new CoalMine();
+            minesList.add(coalMine);
+        }
+    }
+    public void addCoalPlants(int numberOfPlants){
+        for (int i=0; i <= numberOfPlants; i++){
+            CoalPlant coalPlant = new CoalPlant();
+            powerPlants.add(coalPlant);
+        }
+    }
 
 
+    public int getFuelUsedPerMonth(){
+        int fuel = 0;
+        for(Mine mine : minesList){
+            fuel += mine.getFuelUsed();
+        }
+        return fuel * 30;
+    }
+    public int getEnergyProduced(){
+        int fuel = 0;
+        for (PowerPlant powerPlant : powerPlants){
+            fuel += powerPlant.getEnergyProduced();
+        }
+        return fuel * 30;
+    }
+    public int getConsumedFuel(){
+        int fuel = 0;
+        for (PowerPlant powerPlant : powerPlants){
+            fuel += powerPlant.getFuelTaken();
+        }
+        return fuel * 30;
+    }
+
+
+    public void addUraniumMine(int numberOfMines) {
+        for (int i=0; i <= numberOfMines; i++){
+            UraniumMine uraniumMine = new UraniumMine();
+            minesList.add(uraniumMine);
+        }
+    }
+
+    public void addSolarPlants(int numberOfPlants) {
+        for (int i=0; i <= numberOfPlants; i++){
+            SolarPlant solarPlant = new SolarPlant();
+            powerPlants.add(solarPlant);
+        }
+    }
 }
