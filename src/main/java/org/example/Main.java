fc83fc64132c0bfe8126cee8e0802d287d3a4303
Piotr Petryka <ppwxp19@gmail.com>
2023-01-24 14:09:01 +0100

    Moving energyconsumers enum to consumers package

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.powerplants.CoalPlant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        CoalPlant coalPlant = new CoalPlant();
+        System.out.println(coalPlant.getEnergyProduced());
     }
 }
--- src/main/java/org/example/consumers/EnergyConsumers.java
+++ src/main/java/org/example/consumers/EnergyConsumers.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.consumers;
 
 public enum EnergyConsumers {
     WORLD(6000000), POLAND(40000), NEW_YORK(10000);
