6a6fc2fb9c6616d424327bb1730c0320ee2fd9df
Piotr Petryka <ppwxp19@gmail.com>
2023-02-08 18:36:22 +0100

    refactoring

--- src/main/java/org/example/mines/CoalMine.java
+++ src/main/java/org/example/mines/CoalMine.java
@@ -3,10 +3,7 @@ package org.example.mines;
 public class CoalMine extends Mine{
     public CoalMine() {
         super(MinesEnum.CoalMine.getEnergy());
-        System.out.println("to z maina");
-        System.out.println("z maina 2dfff");
-        System.out.println("test");
-        System.out.println("z ma");
         System.out.println("bugfix");
+
     }
 }
